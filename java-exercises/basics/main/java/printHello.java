9bdc50ea89b309099712cba6dac41117d72c0c77
Jlee108 <jbacked@yahoo.com>
2022-02-13 20:35:53 +0000

    First exercise

---
+++ java-exercises/basics/main/java/printHello.java
@@ -0,0 +1,14 @@
+/*
+1. Write a Java program to print 'Hello' on screen and then print your name on a separate line. Go to the editor
+Expected Output :
+Hello
+Alexandra Abramov
+
+ */
+
+public class printHello {
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
